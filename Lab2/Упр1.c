1ce9e95a0a031c48a084337ffac972e0cddb3a2f
dev9tka62 <54841902+dev9tka62@users.noreply.github.com>
2019-10-10 23:31:48 +0300

    Lab2

---
+++ Lab2/Упр1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a, b=10, c=2, r;
+printf ("Vvedi a:");
+scanf("%d", &a);
+r= b*c;
+if(a>r) printf("ne proydet");
+else if(a<r || a==20) printf("proydet");
+}
---
+++ Lab2/Упр3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int K;
+printf("Vvedite K:");
+scanf("%d", &K);
+switch (K)
+{
+case 1: printf("I"); break;
+case 2: printf ("II"); break;
+case 3: printf ("III"); break;
+case 4: printf ("IV"); break;
+case 5: printf ("V"); break;
+case 6: printf ("VI"); break;
+case 7: printf ("VII"); break;
+case 8: printf ("VIII"); break;
+case 9: printf ("IX"); break;
+case 10: printf ("X"); break;
+default: printf ("Nevernoe znachenie");
+	
+}
+	return 0;
+}
