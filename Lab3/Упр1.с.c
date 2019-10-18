06b7266578dcb515251aa087aa86680b70f55039
dev9tka62 <54841902+dev9tka62@users.noreply.github.com>
2019-10-18 15:21:05 +0300

    Update Упр1.с.c

--- Lab3/Упр1.с.c
+++ Lab3/Упр1.с.c
@@ -1,15 +1,39 @@
-#include <stdlib.h>
-int main(int argc, char *argv[]) {
-	int m, k=5, d, t=3, v=1, c, h;
-	printf("Vvedite m:");
-	scanf("%d", &m);
-	if(m>50) return 1;
-	printf("P9tiryblevix: %d\n", m/k);
-	d=m%t;
-	printf("Trexryblevix: %d\n", d);
-	c=d/v;
-	printf("Odnaryblevix: %d\n", c);
-	if((m/k)+d+c>=10) printf("Nel`z9 razmen9t`");
-	else printf("mohno");
-	return 0;
+#include<stdio.h>
+void main()
+{
+int n, a,b,c,t=5,y=3,u=1;
+while(1) {
+printf("meny:\n");
+printf("1 – skol`ko 5p\n ");
+printf("2 – skol`ko 3p\n ");
+printf("3 – skol`ko 1p\n ");
+printf("4 – vixod is programmi\n ");
+printf("vash vibor?:");
+scanf("%d", &n);
+switch (n)
+{
+case 1:
+printf("vvedite skol`ko rybley:\n ");
+scanf("%d", &a);
+if(a>50) printf ("Vvedite znachenie <50\n");
+else if(a<50) printf("P9tiryblevix: %d\n ", a/t);
+break;
+case 2: 
+printf("vvedite skol`ko rybley:\n ");
+scanf("%d", &b);
+if(b>50)printf ("Vvedite znachenie <50\n");
+else if(a<50) printf("Trexryblevix: %d\n ", b/y);
+break;
+case 3: 
+printf("vvedite skol`ko rybley:\n");
+scanf("%d",&c);
+if(c>50) printf ("Vvedite znachenie <50\n");
+else if(a<50) printf("Odnoryblevix:%d\n", c/u);
+break;
+if((a/t) || (b/y) || (c/u) >=10) printf("Nel`z9 razmen9t`");
+case 4:
+return; break;
+default: printf("Nevernoe zna4enie\n");
+}
+}
 }
